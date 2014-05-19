aa3ecc9a78d4fd1e00244e9fe262d2599e9ca5e7
Raphaël Barrois <raphael.barrois@polytechnique.org>
2014-05-19 22:54:55 +0200

    Normalize setup.py

--- setup.py
+++ setup.py
@@ -1,29 +1,68 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Copyright (c) 2011-2013 Raphaël Barrois
+# This code is distributed under the two-clause BSD License.
 
-from setuptools import setup, find_packages
+import codecs
+import os
+import re
+import sys
+
+from setuptools import setup
+
+root_dir = os.path.abspath(os.path.dirname(__file__))
+
+
+def get_version(package_name):
+    version_re = re.compile(r"^__version__ = [\"']([\w_.-]+)[\"']$")
+    package_components = package_name.split('.')
+    init_path = os.path.join(root_dir, *(package_components + ['__init__.py']))
+    with codecs.open(init_path, 'r', 'utf-8') as f:
+        for line in f:
+            match = version_re.match(line[:-1])
+            if match:
+                return match.groups()[0]
+    return '0.1.0'
+
+
+PACKAGE = 'django_factory_boy'
+
 
 setup(
     name='django-factory_boy',
-    version='0.1.7', # remember to change django_factory_boy/__init__.py
+    version=get_version(PACKAGE),
+    description="Uses factory_boy to supply test data factory classes for all stock Django models.",
     author='Votizen',
     author_email='team@votizen.com',
-    url='http://github.com/votizen/django-factory_boy',
-    description = 'Uses factory_boy to supply test data factory classes for all stock Django models.',
-    packages=find_packages(),
-    zip_safe=False,
+    maintainer='Raphaël Barrois',
+    maintainer_email='raphael.barrois+fboy@polytechnique.org',
+    url='http://github.com/rbarrois/django-factory_boy',
+    packages=[PACKAGE],
     install_requires=['factory_boy>=1.0.4'],
-    include_package_data=True,
+    license="BSD",
+    setup_requires=[
+        'setuptools>=0.8',
+    ],
     tests_require=[
-        'unittest2>=0.5.1',
-        'django>=1.3',
+        'Django>=1.3',
         'factory_boy>=1.0.4'
     ],
-    test_suite='runtests.runtests',
     classifiers=[
-        'Framework :: Django',
-        'Intended Audience :: Developers',
-        'Operating System :: OS Independent',
-        'Topic :: Software Development'
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "Framework :: Django",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.2",
+        "Programming Language :: Python :: 3.3",
+        "Topic :: Software Development :: Testing",
+        "Topic :: Software Development :: Libraries :: Python Modules",
     ],
+    test_suite='runtests.runtests',
 )
 
